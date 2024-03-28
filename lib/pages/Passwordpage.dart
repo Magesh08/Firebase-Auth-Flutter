0293d272ea8d5bf1ea7340003f668fda621aced1
Magesh08 <mageshvaran16@gmail.com>
2024-03-28 14:13:39 +0530

    gg sign in

--- lib/pages/Passwordpage.dart
+++ lib/pages/Passwordpage.dart
@@ -1,32 +1,58 @@
-import 'dart:ui';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:loginauth/components/textfieldui.dart';
 
 class passwordpage extends StatefulWidget {
   final Function()? onTap;
 
-  const passwordpage({super.key, required this.onTap});
+  const passwordpage({Key? key, required this.onTap}) : super(key: key);
 
   @override
   State<passwordpage> createState() => _passwordpageState();
 }
 
 class _passwordpageState extends State<passwordpage> {
-  void signin() async {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        });
-  }
-
   final usernamecontrol = TextEditingController();
   final passwordcontrol = TextEditingController();
+
+  Future<void> _signInWithGoogle(BuildContext context) async {
+    try {
+      final GoogleSignIn googleSignIn = GoogleSignIn();
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+
+      // Trigger the Google Sign In flow
+      final GoogleSignInAccount? googleSignInAccount =
+          await googleSignIn.signIn();
+
+      if (googleSignInAccount != null) {
+        // Obtain the GoogleSignInAuthentication object
+        final GoogleSignInAuthentication googleSignInAuthentication =
+            await googleSignInAccount.authentication;
+
+        // Create a new credential
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+        );
+
+        // Sign in to Firebase with the Google credential
+        final UserCredential authResult =
+            await _auth.signInWithCredential(credential);
+        final User? user = authResult.user;
+
+        if (user != null) {
+          // Handle successful sign-in, such as navigating to the next screen
+          print('User signed in with Google: ${user.displayName}');
+        }
+      } else {
+        // User cancelled the Google sign in process
+        print('User cancelled sign-in');
+      }
+    } catch (error) {
+      print('Error signing in with Google: $error');
+    }
+  }
 
   void signuserId(BuildContext context) async {
     try {
@@ -74,65 +100,72 @@ class _passwordpageState extends State<passwordpage> {
             width: MediaQuery.of(context).size.width * 0.8,
             child: SingleChildScrollView(
               child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(
-                      Icons.credit_card,
-                      size: 80,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.credit_card,
+                    size: 80,
+                  ),
+                  Text(
+                    'Welcome to Sign In',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                    Text(
-                      'welcome to sign In',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+                  ),
+                  Textfieldfun(
+                    controller: usernamecontrol,
+                    hintText: 'Enter your email',
+                    obscuretext: false,
+                    icon: Icons.mail_rounded,
+                  ),
+                  Textfieldfun(
+                    controller: passwordcontrol,
+                    hintText: 'Enter the password',
+                    obscuretext: true,
+                    icon: Icons.password_rounded,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text('Forgot Password'),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      signuserId(context);
+                    },
+                    child: Text('Sign In'),
+                  ),
+                  Text('----or continue with----'),
+                  GestureDetector(
+                    onTap: () {
+                      _signInWithGoogle(context);
+                    },
+                    child: Icon(Icons.access_alarm_rounded),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Not a user?',
+                        style: TextStyle(fontSize: 16, color: Colors.black),
                       ),
-                    ),
-                    Textfieldfun(
-                      controller: usernamecontrol,
-                      hintText: 'Enter your gmail id',
-                      obscuretext: false,
-                      icon: Icons.mail_rounded,
-                    ),
-                    Textfieldfun(
-                      controller: passwordcontrol,
-                      hintText: 'Enter the password',
-                      obscuretext: true,
-                      icon: Icons.password_rounded,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text('Forgot Password'),
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child: Text(
+                          'Register',
+                          style:
+                              TextStyle(fontSize: 16, color: Colors.lightBlue),
+                        ),
                       ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        signuserId(context);
-                      },
-                      child: Text('Sign In'),
-                    ),
-                    Text('----or continue with----'),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'not an user ?',
-                          style: TextStyle(fontSize: 16, color: Colors.black),
-                        ),
-                        GestureDetector(
-                          onTap: widget.onTap,
-                          child: Text(
-                            'Register',
-                            style: TextStyle(
-                                fontSize: 16, color: Colors.lightBlue),
-                          ),
-                        ),
-                      ],
-                    )
-                  ]),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
